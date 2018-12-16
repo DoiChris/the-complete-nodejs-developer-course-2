2166ffe28c91c749c1ee4f4d56e4c03e53976460
DoiChris <doi@passivenergie.co.jp>
2018-12-16 22:37:04 +0900

    using reqire with async function

--- n-3-2-module-basics/app.js
+++ n-3-2-module-basics/app.js
@@ -5,4 +5,13 @@ const os = require('os');
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`);
+fs.appendFile(`greetings.txt`, `Hello World!`, (err)=>{
+    if (err){
+        console.log(`unable to write to file`);
+    }
+});
+
+console.log(user);
+
+var currentOS = os.arch();
+console.log(currentOS);
